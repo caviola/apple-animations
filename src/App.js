1f72f40841af746e165dbc17c961d9a2a25f3898
Albert Almeida <caviola@gmail.com>
2020-04-10 16:08:30 -0400

    Use different transitions for different pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { cloneElement } from "react";
+import React, { cloneElement, useRef, useEffect } from "react";
 import { BrowserRouter, Route, Switch, withRouter } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import { createBrowserHistory } from "history";
 import "./App.scss";
 import IPhone from "./pages/products/IPhone";
 import MacBookPro from "./pages/products/MacBookPro";
@@ -11,20 +10,60 @@ import Logout from "./pages/Logout";
 
 const transitionDuration = 700; // milliseconds
 
-const transitionGroupChildFactory = (child) => {
-  return cloneElement(child, {
-    classNames: "slide-from-right",
-  });
-};
-
-// Add class to setup page-specific entering animations.
+// When a page is about to be transitioned into view, we add a class
+// so that page-specific animations are setup.
 const cssTransitionEnter = (elem) => elem.classList.add("appear");
 
-// Remove class so that entering animations begin.
+// As soon as page starts transitioning into view, we remove the class
+// so that entering animations begin.
 const cssTransitionEntering = (elem) => elem.classList.remove("appear");
 
-const AppRoutes = withRouter(({ location, history }) => {
-  //console.log(history);
+function usePrevious(value) {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = value;
+  });
+
+  return ref.current;
+}
+
+const AppRoutes = withRouter(({ location }) => {
+  // We'll use the previous path to determine the transition to use
+  // when navigating to a new product page.
+  // Bellow is the transition table that we're using. Rows are 'origin' paths
+  // and columns are 'destination' paths.
+  //
+  //               +----------+-------------+-------+
+  //               | iPhone   | MacBook Pro | Watch |
+  // +-------------+----------+-------------+-------+
+  // | iPhone      |          | Fade        | Fade  |
+  // | MacBook Pro | Slide Up |             | Fade  |
+  // | Watch       | Slide Up | Slide Left  |       |
+  // +-------------+----------+-------------+-------+
+  //
+  // In the above table we can see that, for example, the iPhone page always slides up.
+  //
+  const referer = usePrevious(location.pathname);
+
+  // Used to determine which classNames to use in CSSTransition
+  // depending on the origin/destination paths.
+  const transitionGroupChildFactory = (child) => {
+    let classNames;
+
+    if (location.pathname === "/macbook-pro") {
+      classNames = referer === "/iphone" ? "cross-fade" : "slide-from-right";
+    } else if (location.pathname === "/iphone") {
+      classNames = "slide-from-bottom";
+    } else {
+      classNames = "cross-fade";
+    }
+
+    return cloneElement(child, {
+      classNames,
+    });
+  };
+
   return (
     <TransitionGroup
       childFactory={transitionGroupChildFactory}
@@ -49,11 +88,9 @@ const AppRoutes = withRouter(({ location, history }) => {
   );
 });
 
-const history = createBrowserHistory();
-
 const App = () => {
   return (
-    <BrowserRouter history={history}>
+    <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
   );
