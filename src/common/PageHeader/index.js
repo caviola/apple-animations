1f5c72d2a90e266af85b028b355cec20552e64ce
Albert Almeida <caviola@gmail.com>
2020-04-15 19:03:22 -0400

    test

--- src/common/PageHeader/index.js
+++ src/common/PageHeader/index.js
@@ -1,7 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import cx from "classnames";
-import styles from "./styles.module.scss";
+import { StyleSheet, css } from "aphrodite/no-important";
+
+const syles = StyleSheet.create({
+  pageHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingRight: '115px',
+  
+    logo: {
+      display: 'inline-block',
+      width: '100px',
+      height: '100px',
+      background: '#000',
+    },
+  
+    nav: {
+      marginLeft: 'auto',
+    },
+  
+    ul: {
+      listStyleType: 'none',
+      display: 'flex',
+      alignItems: 'center',
+  
+      li: {
+        'li + li': {
+          marginLeft: '51px',
+        },
+  
+        a: {
+          color: '#707070',
+          textDecoration: 'none',
+          cursor: 'pointer',
+  
+          &.active {
+            fontWeight: 'bold',
+            color: '#000',
+          },
+
+          whiteSpace: 'nowrap',
+        },
+  
+        notify: {
+          fontSize: 'inherit',
+          display: 'inline-block',
+          border: 'none',
+          background: '#5ac8fa',
+          color: 'white',
+          padding: '10px 25px',
+          borderRadius: '25px',
+        }
+      }
+    }
+  }  
+});
 
 function PageHeader({ activePath }) {
   return (
