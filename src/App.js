211faa9701d0ac3555fbd8f2c4bf231d97068c85
Albert Almeida <caviola@gmail.com>
2026-04-12 10:37:04 -0400

    Refactor/cleanup code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { cloneElement } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import styles from "./App.module.scss";
 import IPhone from "./pages/products/IPhone";
@@ -10,19 +9,15 @@ import Login from "./pages/Login";
 import RegistrationForm from "./pages/RegistrationForm";
 import { ProtectedRoute } from "./common/session";
 
-const transitionDuration = 700; // milliseconds
+const TRANSITION_DURATION = 700; // milliseconds
 
 // When a page is about to be transitioned into view, we add a class
 // so that page-specific animations are setup.
-function cssTransitionEnter(elem) {
-  return elem && elem.classList.add("appear");
-}
+const cssTransitionEnter = (elem) => elem?.classList.add("appear");
 
 // As soon as page starts transitioning into view, we remove the class
 // so that entering animations begin.
-function cssTransitionEntering(elem) {
-  return elem && elem.classList.remove("appear");
-}
+const cssTransitionEntering = (elem) => elem?.classList.remove("appear");
 
 /**
  * Given origin/destination paths, returns which class name to use for CSSTransition.
@@ -46,7 +41,7 @@ function cssTransitionEntering(elem) {
  * @param {string} toLocation
  */
 function getTransitionClassNames(fromPath, toLocation) {
-  if (toLocation.state.transitionClass) {
+  if (toLocation.state?.transitionClass) {
     return toLocation.state.transitionClass;
   }
 
@@ -56,66 +51,64 @@ function getTransitionClassNames(fromPath, toLocation) {
 
   if (toLocation.pathname === "/iphone") {
     return "slide-from-bottom";
-  } else {
-    return "cross-fade";
   }
+
+  return "cross-fade";
 }
 
 const AppRoutes = () => {
   const location = useLocation();
-  const referer = location.state && location.state.referer;
+  const referer = location.state?.referer;
+  const shouldAnimate =
+    referer !== location.pathname && location.state?.animate;
 
-  function transitionGroupChildFactory(child) {
-    if (
-      referer !== location.pathname &&
-      location.state &&
-      location.state.animate
-    ) {
+  const transitionGroupChildFactory = (child) => {
+    if (shouldAnimate) {
       // We were requested to animate the transition from one page to the other,
       // so setup the child element (CSSTransition) with the appropriate animation class.
       return cloneElement(child, {
-        timeout: transitionDuration,
+        timeout: TRANSITION_DURATION,
         classNames: getTransitionClassNames(referer, location),
         appear: true,
         onEnter: cssTransitionEnter,
         onEntering: cssTransitionEntering,
       });
-    } else {
-      // No animation.
-      return cloneElement(child, {
-        timeout: 0,
-        classNames: "no-anim",
-        appear: false,
-        onEnter: null,
-        onEntering: null,
-      });
     }
-  }
+
+    // No animation.
+    return cloneElement(child, {
+      timeout: 0,
+      classNames: "no-anim",
+      appear: false,
+      onEnter: null,
+      onEntering: null,
+    });
+  };
 
   return (
     <TransitionGroup
       childFactory={transitionGroupChildFactory}
       className={styles.container}
     >
-      <CSSTransition key={location.key} timeout={0}>
+      <CSSTransition key={location.key}>
         <Routes location={location}>
-          <Route exact path="/" element={
+          <Route path="/" element={
             <Login />
           } />
-          <Route exact path="/register" element={
+          <Route path="/register" element={
             <RegistrationForm />
           } />
-          <Route exact path="/iphone" element={
+          <Route path="/iphone" element={
             <ProtectedRoute>
               <IPhone />
             </ProtectedRoute>
           } />
-          <Route exact path="/macbook-pro" element={
+          <Route path="/macbook-pro" element={
             <ProtectedRoute>
               <MacBookPro />
             </ProtectedRoute>
           } />
-          <Route exact path="/watch" element={
+          <Route path="/watch" element={
             <ProtectedRoute>
               <Watch />
             </ProtectedRoute>
